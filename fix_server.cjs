15b33b3c134f4845cad657dd5a7f9fa977213a33
Cairo Jose <cairo91197503@gmail.com>
2026-07-07 00:21:56 -0300

    chore: removepatch scripts obsoletos

--- fix_server.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('server.ts', 'utf8');
-
-content = content.replace(
-  'app.get("/api/gmb/locations/:locationId/reviews", async (req, res) => {',
-  'app.get("/api/gmb/locations/reviews", async (req, res) => {'
-);
-fs.writeFileSync('server.ts', content);
-console.log("Fixed reviews route in server.ts");
